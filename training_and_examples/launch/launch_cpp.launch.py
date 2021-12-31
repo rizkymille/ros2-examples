7a9a6215d317f7991c048f9e70f0e93cfc4c5539
rizkymille <muhammad.rizky912@ui.ac.id>
2022-01-01 00:56:00 +0700

    Add global_param trick

--- training_and_examples/launch/launch_cpp.launch.py
+++ training_and_examples/launch/launch_cpp.launch.py
@@ -4,10 +4,17 @@ from launch_ros.actions import Node
 def generate_launch_description():
   ld = LaunchDescription()
 
+  # cara bikin global parameter tanpa global parameter server,
+  # using vtol e p i c big brain technique
+  # glob_params = [{"lala/lele":"LOLO"},
+  #                {"haha/hihi":"HEHO"}]
+
   listener_server_node = Node(
     package='training_and_examples',
     executable='listener_server_cpp',
     name='listener_server',
+    # biar bisa masukkin global param:
+    # parameters=glob_params,
     output='screen'
   )
 
@@ -15,6 +22,8 @@ def generate_launch_description():
     package='training_and_examples',
     executable='talker_client_cpp',
     name='talker_client',
+    # biar bisa masukkin global param:
+    # parameters=glob_params+[{"example_param/Input":"PRINT"}],
     parameters=[{"example_param/Input":"PRINT"}],
     output='screen'
   )
